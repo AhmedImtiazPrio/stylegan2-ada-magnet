0f6e4a6e3e5a25da91695cdb4eab16231569ab02
AhmedImtiazPrio <ahmed.imtiaz.prio@gmail.com>
2022-06-29 13:05:11 -0500

    svd calculation and extraction

---
+++ compile_svds_tf1.py
@@ -0,0 +1,45 @@
+import tqdm
+import numpy as np
+import sys
+
+if len(sys.argv)<3:
+    raise AttributeError('''
+    Usage:
+    python save_filename conditional_flag regexp_path_to_npz
+    ''')
+    
+save_file = sys.argv[1]
+conditional_flag = sys.argv[2]
+filenames = sys.argv[3:]
+
+svds = []
+latents = []
+labels = []
+
+for each_file in tqdm.tqdm(filenames):
+    with np.load(each_file) as data:
+        svd = data['svds']
+        latent = data['latents']
+        
+        if conditional_flag:
+            label = data['labels']
+        
+    svds.append(svd.reshape(-1,svd.shape[-1]))
+    latents.append(latent.reshape(-1,latent.shape[-1]))
+    
+    if conditional_flag:
+        labels.append(label.reshape(-1,label.shape[-1]))
+    
+    
+svds = np.concatenate(svds,axis=0)
+latents = np.concatenate(latents,axis=0)
+
+if conditional_flag:
+    labels = np.concatenate(labels,axis=0)
+
+print(svds.shape)
+print(latents.shape)
+print(labels.shape)
+
+np.savez(save_file,latents=latents,svds=svds,labels=labels)
+    
--- get_svds_tf1.py
+++ get_svds_tf1.py
@@ -14,13 +14,39 @@ import tqdm
 import sys
 import pickle
 import os
+import argparse
 
-minibatch_per_gpu = int(sys.argv[1])
-num_gpu = int(sys.argv[2])
-truncation_psi = 1
-num_latents = int(sys.argv[3])
-network_path = str(sys.argv[4])
-save_path = str(sys.argv[5])
+
+parser = argparse.ArgumentParser(
+        description='calculate singular values for a given pretrained model',
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+parser.add_argument('--batch_size', help='minibatch per gpu', type=int, default=16)
+parser.add_argument('--num_gpu', help='number of gpus', type=int, default=1)
+parser.add_argument('--truncation_psi', help='truncation for stylegan', type=float, default=1)
+parser.add_argument('--N', help='number of latents to sample', type=int)
+parser.add_argument('--network', help='path to pretrained network pkl', type=str)
+parser.add_argument('--save_path', help='path to save svds', type=str)
+parser.add_argument('--label_size', help='number of labels for conditional sampling', type=int, default=0)
+parser.add_argument('--proj_dim', help='projection dimensions', type=int, default=128)
+
+args = parser.parse_args()
+
+minibatch_per_gpu = args.batch_size
+num_gpu = args.num_gpu
+truncation_psi = args.truncation_psi
+num_latents = args.N
+network_path = args.network
+save_path = args.save_path
+label_size = args.label_size
+proj_svd = args.proj_dim
+
+# minibatch_per_gpu = int(sys.argv[1])
+# num_gpu = int(sys.argv[2])
+# truncation_psi = 1
+# num_latents = int(sys.argv[3])
+# network_path = str(sys.argv[4])
+# save_path = str(sys.argv[5])
 
 if not os.path.exists(save_path):
     os.mkdir(save_path)
@@ -30,7 +56,7 @@ with tf.Graph().as_default(), tflib.create_session().as_default():
     with dnnlib.util.open_url(network_path) as f:
         _G, _D, Gs = pickle.load(f)
     
-    proj_svd = 128
+#     proj_svd = 128
     output_flatten = np.prod(Gs.output_shape[1:])
     
     init = tf.keras.initializers.Orthogonal(gain=1.0, seed=0)
@@ -45,24 +71,30 @@ with tf.Graph().as_default(), tflib.create_session().as_default():
             Gs_clone = Gs.clone()
 
             latents_in = tf.random_normal([minibatch_per_gpu] + Gs_clone.input_shape[1:])
-            labels = None
+            labels_in = tf.zeros([minibatch_per_gpu, 0])
             
-            images = Gs_clone.get_output_for(latents_in, labels,
+            if label_size:
+                labels_in = tf.gather(tf.eye(label_size), tf.random.uniform([minibatch_per_gpu], 0, label_size, dtype=tf.int32))
+                
+            images = Gs_clone.get_output_for(latents_in, labels_in,
                                              truncation_psi=truncation_psi, is_validation=True)
 
+#             result_expr.append([images,latents_in,labels_in]) ## debug
+            
             images = tf.reshape(images,shape=(minibatch_per_gpu,output_flatten))
             proj_image = tf.matmul(images, w, transpose_b=True)
 
             jacobian = batch_jacobian(proj_image, latents_in, use_pfor=False)
             svd = tf.linalg.svd(jacobian, full_matrices=False, compute_uv=False)
 
-            result_expr.append([svd,latents_in])
+            result_expr.append([svd,latents_in,labels_in])
 
     
     for it in tqdm.tqdm(range(0,num_latents//1000),desc='saving loop'):
         
         svds = []
         latents = []
+        labels = []
 
         for itt in tqdm.tqdm(range(1000),desc='sampling loop'):
             
@@ -71,9 +103,16 @@ with tf.Graph().as_default(), tflib.create_session().as_default():
             for each in res:
                 svds.append(np.asarray(each[0]))
                 latents.append(np.asarray(each[1]))
+                
+                if label_size:
+                    labels.append(np.asarray(each[2]))
 
         print('saving...')
         latents = np.stack(latents)
+        
+        if label_size:
+            labels = np.stack(labels)
+        
         print(latents.shape)
         print(f'Latents range {latents.max()} {latents.min()}')
         svds = np.stack(svds)
@@ -83,5 +122,6 @@ with tf.Graph().as_default(), tflib.create_session().as_default():
         np.savez(
             os.path.join(save_path,f"{''.join(network_path.split('.')[:-1])}_{it}_{str(datetime.now()).replace(' ','-')}.npz"),
             svds=svds,
-            latents=latents
+            latents=latents,
+            labels=labels
         )
