77a74e25d447118d81d88b2d31663a9a1900293b
AhmedImtiazPrio <ahmed.imtiaz.prio@gmail.com>
2022-07-25 05:34:03 -0500

    sampler docs

--- polarity_utils.py
+++ polarity_utils.py
@@ -1,29 +1,69 @@
+import numpy as np
+import warnings
+
 class polSampler(object):
     """
     Sampler class for polarity sampling
-    latents (np.ndarray): latent vectors or random seeds for corresponding latents
-    svds (np.ndarray): singular values of the input-output transformation corresponding to latent vector locations
-    labels (np.ndarray): one hot encoded labels for conditional generators
+    
+    Usage:
+    =====
+    # For conditional sampling
+    
+    ## Initialize sampler
+    sampler = polSampler('./svds/cifar10_pixel.npz',rho=1,top_k=10)
+    
+    ## Sample with initialized polarity, equal number of samples from each class
+    sampled_lat,sampled_cond = sampler.sample(n=20)
+    
+    ## Update sampling polarity and sample conditioned on class
+    sampler.update_rho(-.3)
+    sampled_lat,sampled_cond = sampler.sample(n=20,conditional_class=2)
+    
+    # For unconditional sampling
+    ## Initialize sampler
+    sampler = polSampler('./svds/ffhq_pixel.npz',rho=1,top_k=10)
+    
+    ## Sample with initialized polarity
+    sampled_lat = sampler.sample(n=20)
+    
+    ## Update sampling polarity
+    sampler.update_rho(-.3)
+    sampled_lat = sampler.sample(n=20)    
+    
     """
-    def __init__(self,singulars_npz,rho,top_k=30,is_conditional=False):
+    def __init__(self,singulars_npz_path,rho,top_k=30,is_conditional=False,verbose=True):
+    """
+    singulars_npz_path (str): path to numpy file containing the following:
+        latents (np.ndarray): latent vectors or random seeds/states for corresponding latents
+        svds (np.ndarray): singular values of the input-output transformation corresponding to latent vector locations
+        labels (np.ndarray): optional, one hot encoded labels for conditional generators
         
-        with np.load(singulars_npz) as data:
+    rho (float): polarity parameter rho
+    top_k (int): top singulars to use for volume scalar estimation
+    is_conditional (bool): conditional generator flag
+    """    
+        
+        with np.load(singulars_npz_path) as data:
             
             self.svds = data['svds']
             self.latents = data['latents']
             
-            if is_conditional: ## load saved condition vectors
+            self.labels = None
+            
+            if 'labels' in [each for each in data.keys()]: ## if has stored condition vectors, legacy support
+                
                 self.labels = data['labels']
-            else:
-                self.labels = None
-        
-#         self.svds = svds
-#         self.latents = latents
-
+                
+                if self.labels == []:  ## hasattr but attr is []
+                    self.labels = None
+                    assert not is_conditional ## condition data not found in npz
+                else:
+                    is_conditional = True ## condition data found in npz
+                    warnings.warn('Condition data found in npz file, setting `is_conditional=True`')
+                    
         self.is_conditional = is_conditional
         self.top_k = top_k
         self.rho = rho
-#         self.labels = labels
         
         # for conditional
         if labels is not None:
@@ -34,11 +74,9 @@ class polSampler(object):
             self.class_labels = np.argmax(labels,axis=-1)
             self.unique_classes = np.unique(self.class_labels)
             self.num_classes = labels.shape[1]
-#             self.is_conditional = True
             
         else:
             self.classes = None
-#             self.is_conditional = False
         
         ## get unnormalized probabilities
         sigma = np.exp(np.log(self.svds[:,:self.top_k].astype(np.float64)).sum(1))
@@ -59,7 +97,12 @@ class polSampler(object):
         
     def sample(self,n,replace=False,conditional_class=None,seed=None):
         """
-        Uses precomputed probabilites based on initialized rho
+        Sample latents (and condition vectors) using precomputed weights
+        
+        n (int): number of samples
+        replace (bool): sample with/without replacing
+        conditional_class (int): optional, class to use for sampling
+        seed (int): optional, random seed for sampling
         """
         
         np.random.seed(seed)
@@ -111,7 +154,7 @@ class polSampler(object):
         
     def update_rho(self,rho):
         """
-        Recomputes probabilites for new rho
+        Recompute probabilites for new rho/polarity
         """
         self.rho = rho
         
@@ -130,3 +173,43 @@ class polSampler(object):
                 idxs = np.where(self.class_labels == each_class)[0]
                 self.classwise_idx.append(idxs)
                 self.proba.append(proba_un[idxs]/np.sum(proba_un[idxs]))
+                
+def to_uint8(img):
+    '''
+    Convert image to uint8
+    '''
+    img = img*127.5 + 128
+    img = np.clip(img,0,255)
+    return img.astype(np.uint8) 
+
+def imgrid(imarray, cols=10, pad=1, pad_value=0):
+    '''
+    Display image array in a grid
+    
+    imarray (np.uint8): array of numpy uint8 images
+    cols (int): number of columns in final image grid
+    pad (int): pixel padding
+    pad_value (int): 0-255, value to pad with
+    '''
+    if imarray.dtype != np.uint8:
+        raise ValueError('imgrid input imarray must be uint8')
+    pad = int(pad)
+    assert pad >= 0
+    cols = int(cols)
+    assert cols >= 1
+    N, H, W, C = imarray.shape
+    rows = N // cols + int(N % cols != 0)
+    batch_pad = rows * cols - N
+    assert batch_pad >= 0
+    post_pad = [batch_pad, pad, pad, 0]
+    pad_arg = [[0, p] for p in post_pad]
+    imarray = np.pad(imarray, pad_arg, 'constant', constant_values=pad_value)
+    H += pad
+    W += pad
+    grid = (imarray
+        .reshape(rows, cols, H, W, C)
+        .transpose(0, 2, 1, 3, 4)
+        .reshape(rows*H, cols*W, C))
+    if pad:
+        grid = grid[:-pad, :-pad]
+    return grid
